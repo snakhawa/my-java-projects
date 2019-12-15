f4eb65634e7976c22fe757a35e2e4cc3a0a9dc7b
Sageer Nakhawa <snakhawa@expedia.com>
2019-12-15 14:00:22 -0800

    1. Practice LinkList examples 2. Leetcode - Merge Sorted Arrays

---
+++ src/com/snakhawa/homework/linkedlist/MergeTwoSortedList.java
@@ -0,0 +1,42 @@
+package com.snakhawa.homework.linkedlist;
+
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
+public class MergeTwoSortedList {
+
+  public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+
+    if(l1 == null) {return l2;}
+    else if(l2 == null) {return l1;}
+
+    ListNode dummy = new ListNode(0);
+    ListNode curr = dummy;
+
+    while(l1 != null && l2 !=null){
+      if(l1.data <= l2.data){
+        curr.next = l1;
+        l1 = l1.next;
+      } else{
+        curr.next = l2;
+        l2 = l2.next;
+      }
+      curr = curr.next;
+    }
+
+    if(l1 == null){
+      curr.next = l2;
+    } else{
+      curr.next = l1;
+    }
+
+    return dummy.next;
+  }
+
+}
---
+++ src/com/snakhawa/homework/linkedlist/Practice.java
@@ -0,0 +1,203 @@
+package com.snakhawa.homework.linkedlist;
+
+import java.util.List;
+
+public class Practice {
+
+  /**
+   * list -> [ 1 ] -> [ 2 ]    TO  list -> [ 1 ] -> [ 2 ] -> [ 3 ]
+   */
+  public static void addNodeToTail_9(){
+    ListNode head = new ListNode(1, new ListNode(2));
+    head.next.next = new ListNode(3);
+    printNode(head);
+  }
+
+  /**
+   * list -> [ 1 ] -> [ 2 ] -> null    TO  list -> [ 3 ] -> [ 1 ] -> [ 2 ] -> null
+   */
+  public static void addNodeToHead_10(){
+    ListNode head = new ListNode(1, new ListNode(2));
+    ListNode n3 = new ListNode(3, head);
+    printNode(n3);
+  }
+
+  /**
+   * list -> [ 1 ] -> [ 2 ] -> null    TO  list -> [ 1 ] -> [ 3 ] -> [ 4 ] --> [ 2 ] -> null
+   */
+  public static void insertNodeBetweenNodes_11(){
+    ListNode head = new ListNode(1, new ListNode(2));
+    ListNode n34 = new ListNode(3, new ListNode(4));
+    n34.next.next = head.next;
+    head.next = n34;
+    printNode(head);
+  }
+
+  /**
+   * list -> [ 1 ] -> [ 2 ] -> [ 3 ] --> null
+   *      TO
+   * list -> [ 2 ] -> null
+   * &
+   * list2 -> [ 1] -> [ 3 ] -> null
+   */
+  public static void breakList_12(){
+    ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
+    ListNode list2 = head; //  1 - 2 - 3
+    head = head.next; // list -> 2
+    list2.next = list2.next.next; // 1 -> 3
+    head.next = null; // 2
+
+    printNode(head);
+    printNode(list2);
+
+  }
+
+  /**
+   * list -> [ 5 ] -> [ 4 ] -> [ 3 ] --> null
+   *      TO
+   * list -> [ 4] -> [ 5 ] -> [ 3 ] -> null
+   */
+  public static void swapNode_13(){
+    ListNode head = new ListNode(5, new ListNode(4, new ListNode(3)));
+    ListNode node4 = head.next; //[ 4 ] -> [ 3 ] --> null
+    head.next = head.next.next; //[ 5] -> [ 3 ] -> null
+    node4.next = head; //[4] -> [ 5 ] -> [ 3 ] --> null
+    head = node4; //[ 2 ] --> null
+    printNode(head);
+
+  }
+
+  /**
+   * list -> [ 5 ] -> [ 4 ] -> [ 3 ] --> null
+   *      TO
+   * list -> [ 3 ] -> [ 4 ] -> [ 5 ] -> null
+   */
+  public static void reverse_14(){
+    ListNode head = new ListNode(5, new ListNode(4, new ListNode(3)));
+    ListNode prevNode = null;
+    ListNode currNode = head;
+    ListNode followNode = head;
+    while (currNode != null){
+      followNode = followNode.next;
+      currNode.next = prevNode;
+      prevNode = currNode;
+      currNode = followNode;
+    }
+    printNode(prevNode);
+
+  }
+
+  /**
+   * list -> [ 1 ] -> [ 2 ] -> [ 3 ] --> null
+   * temp -> [ 4 ] -> [ 5] -> [ 6 ] --> null
+   *      TO
+   * list -> [ 5 ] -> [ 3 ] -> [ 4 ] -> [ 2 ] -> null
+   */
+  public static void merge_15(){
+    ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
+    ListNode temp = new ListNode(4, new ListNode(5, new ListNode(6)));
+
+    ListNode n5 = temp.next; // [5]
+    temp.next = head.next; // 4 - 2 - 3 - null
+    n5.next = head.next.next; // 5 - 3 - null
+    temp.next.next = null; // 4 - 2 - null
+    head = n5; // 5 - 3 - null
+    head.next.next = temp; // 5 - 3 - 4 - 2 - null
+
+    printNode(head); // [ 5 ]-->[ 3 ]-->[ 4 ]-->[ 2 ]-->
+
+  }
+
+  /**
+   * list -> [ 1 ] -> null
+   * list2 -> [ 2] -> [ 3] -> [ 4 ] --> null
+   *      TO
+   * list -> [ 4 ] -> [ 1 ] -> [ 2 ] --> null
+   * &
+   * list2 -> [ 3 ] -> null
+   */
+  public static void problem_16(){
+    ListNode list = new ListNode(1, null);
+    ListNode list2 = new ListNode(2, new ListNode(3, new ListNode(4)));
+    list.next = list2; // 1 -> 2 -> 3 -> 4 -> null
+    list2 = list2.next; // 3 -> 4 - null
+    list.next.next = null; // 1 -> 2 -> null
+    ListNode n4 = list2.next; // 4 -> null
+    list2.next = null; // 3 -> null
+    n4.next = list; // 4 -> 1 -> 2 -> null
+    list = n4;
+
+    printNode(list);
+    printNode(list2);
+
+  }
+
+  /**
+   * list -> [ 1 ] -> [ 2] -> [ 3] -> [ 4 ] --> null
+   *      TO
+   * list -> [ 4 ] -> [ 2 ] --> null
+   * &
+   * list2 -> [ 3 ] -> [ 1 ] -> null
+   */
+  public static void problem_17(){
+    ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+
+    ListNode even = null;
+    ListNode odd = null;
+    while(head != null){
+      ListNode temp = head.next;
+      if(head.data % 2 == 0){
+        head.next = even;
+        even = head;
+      }else{
+        head.next = odd;
+        odd = head;
+      }
+      head = temp;
+    }
+
+    printNode(odd);
+    printNode(even);
+
+  }
+
+  public static void mergeSortedList(){
+    ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+    ListNode l2 = new ListNode(1, new ListNode(5, new ListNode(6)));
+
+    ListNode merged = new ListNode(0);
+    ListNode cur = merged;
+
+    while(l1 != null && l2 != null){
+      if(l1.data <= l2.data){
+        cur.next= l1;
+        l1 = l1.next;
+      } else{
+        cur.next = l2;
+        l2 = l2.next;
+      }
+      cur = cur.next;
+    }
+
+    if(l1 == null){
+      cur.next= l2;
+    }else {
+      cur.next = l1;
+    }
+
+    printNode(merged.next);
+
+  }
+
+  private static void printNode(ListNode head){
+    StringBuilder sb = new StringBuilder();
+
+    while(head != null){
+      sb.append(String.format("[ %s ]",head.data));
+      sb.append("-->");
+      head = head.next;
+    }
+    System.out.println(sb.toString());
+  }
+
+}
